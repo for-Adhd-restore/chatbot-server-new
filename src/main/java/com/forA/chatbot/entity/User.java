b55c99e0da480d134bea68f6f04d7698219f3e77
ha9eun <ha9eunsoo@gmail.com>
2025-07-26 14:38:56 +0900

    Feat: User 엔터티 생성

---
+++ src/main/java/com/forA/chatbot/entity/User.java
@@ -0,0 +1,37 @@
+package com.forA.chatbot.entity;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 10, nullable = false)
+    private String gender = "UNKNOWN";
+
+    private Integer birthYear;
+
+    @Column(length = 20)
+    private String nickname;
+
+    @Column(length = 20)
+    private String lastName;
+
+    @Column(length = 30)
+    private String firstName;
+
+    @Column(length = 50, nullable = false)
+    private String fullName;
+
+    @Column(name = "is_deleted", nullable = false)
+    private Boolean isDeleted = false;
+
+    @Column(name = "deleated_at")
+    private LocalDateTime deletedAt;
+
+}
