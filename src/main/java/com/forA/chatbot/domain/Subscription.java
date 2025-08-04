3b23adaa50f4a6d7efb011564aeb67ce3eb99a71
ha9eun <ha9eunsoo@gmail.com>
2025-08-04 16:46:15 +0900

    feat: 구독 엔티티 추가

---
+++ src/main/java/com/forA/chatbot/domain/Subscription.java
@@ -0,0 +1,42 @@
+package com.forA.chatbot.domain;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "subscriptions")
+public class Subscription extends BaseTimeEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(name = "product_id", nullable = false, unique = true)
+    private String productId;
+
+    @Column(name = "original_transaction_id", nullable = false)
+    private String originalTransactionId;
+
+    @Column(name = "transaction_id", nullable = false)
+    private String transactionId;
+
+    @Column(name = "start_at", nullable = false)
+    private LocalDateTime startAt;
+
+    @Column(name = "expires_at", nullable = false)
+    private LocalDateTime expiresAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private SubscriptionStatus status;
+
+}
+
---
+++ src/main/java/com/forA/chatbot/domain/SubscriptionStatus.java
@@ -0,0 +1,5 @@
+package com.forA.chatbot.domain;
+
+public enum SubscriptionStatus {
+    ACTIVE, EXPIRED, CANCELLED
+}
