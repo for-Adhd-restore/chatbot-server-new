f79dab3beb7f69bfcbb3e4c90b18fd30d717f88e
ha9eun <ha9eunsoo@gmail.com>
2025-09-23 20:49:36 +0900

    Test: test용 properties를 추가하여 내부DB와 연결

--- src/test/java/com/forA/chatbot/ChatbotApplicationTests.java
+++ src/test/java/com/forA/chatbot/ChatbotApplicationTests.java
@@ -2,10 +2,13 @@ package com.forA.chatbot;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // test 프로파일 사용
 class ChatbotApplicationTests {
 
   @Test
-  void contextLoads() {}
+  void contextLoads() {
+  }
 }
