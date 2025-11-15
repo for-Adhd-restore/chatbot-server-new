827d281b2dde143759feab4dc9202d46c35e07cd
ha9eun <ha9eunsoo@gmail.com>
2025-11-15 17:53:34 +0900

    !BREAKING CHANGE: 구독 상태 varchar로 저장

---
+++ src/main/java/com/forA/chatbot/subscriptions/Subscription.java
@@ -0,0 +1,48 @@
+package com.forA.chatbot.subscriptions;
+
+import com.forA.chatbot.enums.SubscriptionStatus;
+import com.forA.chatbot.global.BaseTimeEntity;
+import com.forA.chatbot.user.domain.User;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "subscriptions")
+public class Subscription extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+  @Column(name = "product_id", nullable = false, unique = true)
+  private String productId;
+
+  @Column(name = "original_transaction_id", nullable = false)
+  private String originalTransactionId;
+
+  @Column(name = "transaction_id", nullable = false)
+  private String transactionId;
+
+  @Column(name = "start_at", nullable = false)
+  private LocalDateTime startAt;
+
+  @Column(name = "expires_at", nullable = false)
+  private LocalDateTime expiresAt;
+
+  @Column(name = "updated_at", nullable = false)
+  private LocalDateTime updatedAt;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, columnDefinition = "VARCHAR")
+  private SubscriptionStatus status;
+
+  @Column(name = "is_auto_renew", nullable = false)
+  private Boolean isAutoRenew;
+
+  @Column(name = "is_trial", nullable = false)
+  private Boolean isTrial;
+}
