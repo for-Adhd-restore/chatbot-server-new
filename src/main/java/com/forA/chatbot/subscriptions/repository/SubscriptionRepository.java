2beaa5e7ef453cd69ae98e39043bffdc9abac82f
LJYeon12 <juyeon010530@gmail.com>
2025-11-16 19:20:14 +0900

    Feat: 구독 조회 api 추가

--- src/main/java/com/forA/chatbot/subscriptions/repository/SubscriptionRepository.java
+++ src/main/java/com/forA/chatbot/subscriptions/repository/SubscriptionRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
   Optional<Subscription> findByOriginalTransactionId(String originalTransactionId);
-  Optional<Subscription> findByUserId(Long userId);
+  Optional<Subscription> findByUser_Id(Long userId);
 }
