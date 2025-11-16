c154e03b41f1d800a098f32b63e4769204c8ce97
LJYeon12 <juyeon010530@gmail.com>
2025-11-16 18:06:45 +0900

    Feat: 채팅 프리미엄 권한 처리

--- src/main/java/com/forA/chatbot/subscriptions/repository/SubscriptionRepository.java
+++ src/main/java/com/forA/chatbot/subscriptions/repository/SubscriptionRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
   Optional<Subscription> findByOriginalTransactionId(String originalTransactionId);
+  Optional<Subscription> findByUserId(Long userId);
 }
